d39a9f8e61223cf30a7e9ceae300e29707b780d6
terry_yu <terry_yu@arcadyan.com.tw>
2022-08-10 15:02:32 +0800

    Add analyze feature
    - create a overall list, monitor_fd_list.csv, to observe number of file
    descriptors in each process clearly.

---
+++ do_analyze.sh
@@ -0,0 +1,141 @@
+# << define function >>
+
+function USAGE(){
+    echo "usage do_analyze.sh"
+}
+
+function FUNC_parse_argument(){
+
+	for arg in $@
+	do
+		case "${arg}" in
+			-h | --help)
+				USAGE
+				exit 0
+				;;
+		esac
+	done
+}
+
+function FUNC_sort(){
+    
+    if [[ $# -ne 1 ]]; then
+        return 1
+    fi
+
+    local src
+    local dst
+    
+    src=$1
+    dst=$(echo $src | tr " " "\n" | sort -n | tr "\n" " ")
+    
+    echo $dst
+}
+
+
+# << main >>
+if [[ $# -ne 0 ]];then
+    FUNC_parse_argument $@
+fi
+
+
+# define directory path
+work_dir="output"
+report_dir="${work_dir}/report"
+
+# make folder and related file
+if [[ -d ${report_dir} ]];then
+    read -p "${report_dir} already existed. overwrite?(y/n) " ans
+    if [[ ${ans} == "y" ]] || [[ ${ans} == "yes" ]]; then
+        echo "rm -rf ${report_dir}"
+        rm -rf ${report_dir}
+    fi
+fi
+
+echo "mkdir ${report_dir}"
+mkdir ${report_dir}
+
+# get monitor output for each process
+# sort file name by ascending value
+files=$(ls ${work_dir}/[0-9]*.csv)
+files=${files//${work_dir}\//}   # replace all "${work_dir}/" to ""
+sorted_files="$(FUNC_sort "${files}")"
+
+# get the total count info
+total_count=$(cat ${work_dir}/log.txt | awk \
+'
+BEGIN{
+    FS=","
+}
+{
+    if ( $1 == "total count" ){
+        split($0, a, "[ ,]");
+        print a[3]
+    }
+}
+'
+)
+
+# define format
+format="%s,%s"
+i=1
+num_arr=""
+while [ ${i} -le ${total_count} ]
+do
+    num_arr="${num_arr} $i"
+    format="${format},%s"
+    echo ${format}
+    i=$(expr $i + 1)
+done
+format="${format}\n"
+echo ${format}
+
+# header
+{
+printf "${format}" "No." "pid" ${num_arr}
+} > "${report_dir}/monitor_fd_list.csv"
+
+#
+i=1
+for file in ${sorted_files}
+do
+
+    file_path="${work_dir}/${file}"
+
+    {
+    cat ${file_path} | awk \
+    -v number=${i} \
+    '
+    BEGIN{
+        FS = ","
+    }
+    $1=="pid"{ pid=$2; }
+    $1=="exe"{ exe=$2; }
+    $1=="cmd"{ cmd=$2; }
+    $1=="total count"{ total_count=$2; }
+    (NR>5)&&(NF==5){
+        count = $3
+        date[ count ] = $1
+        time[ count ] = $2
+        nbr_fds[ count ] = $4
+    }
+    END{
+        printf("%s,", number)
+        printf("%s,", pid)
+        i=1
+        while( i <= total_count ){
+            if ( i in nbr_fds ){
+                printf("%s,", nbr_fds[i])
+            } else {
+                printf("%s,", "-")
+            }
+            i++
+        }
+        print ""
+    }
+    '
+    } >> "${report_dir}/monitor_fd_list.csv" 
+
+    i=$(expr $i + 1)
+done
+
