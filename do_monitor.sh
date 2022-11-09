4064c1de19a7c19a1d2f3ab8eb9cbc26fe06f82b
terry_yu <terry_yu@arcadyan.com.tw>
2022-11-09 16:11:29 +0800

    Add parsing time unit option
    Allow read time unit
    
    e.g.
    
    sh do_monitor --time 1h --period 10s
    sh do_monitor --time 2h --period 10m
    sh do_monitor --time 3d --period 1m

--- do_monitor.sh
+++ do_monitor.sh
@@ -3,12 +3,11 @@
 # << define function >>
 
 function USAGE(){
-    sh_name=${1##*/}
 
-    echo "usage ${sh_name} [-t, --time <time>][-p, --period <period>][-h, --help]"
+    echo "usage $(basename $0) [-t, --time <time>][-p, --period <period>][-h, --help]"
     echo ""
-    echo "<time>    : total time for monitoring process state   (uint:hr) (default: 1hr)"
-    echo "<period>  : sampling rate                             (uint:min)(default: 1min)"
+    echo "<time>    : total time for monitoring process state   (uint:s,m,h,d) (default: 1h)"
+    echo "<period>  : sampling rate                             (uint:s,m,h,d) (default: 1m)"
 }
 
 function FUNC_parse_argument(){
@@ -17,12 +16,12 @@ function FUNC_parse_argument(){
 	do
 		case "${arg}" in
 			-h | --help)
-				USAGE $0 
+				USAGE
 				exit 0
 				;;
 
             -t | --time)
-                target_arg="time" 
+                target_arg="time"
                 ;;
 
             -p | --period)
@@ -32,34 +31,34 @@ function FUNC_parse_argument(){
 			*)
                 if [[ -z ${target_arg} ]]; then
                     echo "error: wrong syntax"
-                    USAGE $0
+                    USAGE
                     exit 1
                 else
                     case "${target_arg}" in
                         time)
-                            if [[ ${arg} -le 0 ]]; then
-                                echo "error: illegal <time> value"
-                                USAGE $0
-                                exit 2
-                            else 
+                            if [[ "${arg}" =~ ^[1-9]+[0-9]*[smhd] ]]; then
                                 time=${arg}
+                            else 
+                                echo "error: ${arg} is illegal <time> value"
+                                USAGE
+                                exit 2
                             fi
                             target_arg=""
 				            ;;
 
                         period)
-                            if [[ ${arg} -le 0 ]]; then
-                                echo "error: illegal <period> value"
-                                USAGE $0
-                                exit 2
-                            else 
+                            if [[ "${arg}" =~ ^[1-9]+[0-9]*[smhd] ]]; then
                                 period=${arg}
+                            else 
+								echo "error: ${arg} illegal <period> value"
+                                USAGE
+                                exit 2
                             fi
                             target_arg=""
                             ;;
                         *)
                             echo "error: wrong syntax"
-                            USAGE $0
+                            USAGE
                             exit 1
                     esac
                 fi
@@ -67,15 +66,66 @@ function FUNC_parse_argument(){
 	done
 }
 
+function FUNC_get_total_count(){
+
+	# define local variables
+	local time="$1"
+	local period="$2"
+	local time_sec
+	local period_sec
+	
+	# transform to sec unit
+	time_sec=$( 
+	echo ${time} | \
+	awk -F"[smhd]" \
+	'
+	/s/{ print $(1) }
+	/m/{ print $(1)*60 }
+	/h/{ print $(1)*60*60 }
+	/d/{ print $(1)*60*60*24 }
+	' 
+	)
+	
+	period_sec=$( 
+	echo ${period} | \
+	awk -F"[smhd]" \
+	'
+	/s/{ print $(1) }
+	/m/{ print $(1)*60 }
+	/h/{ print $(1)*60*60 }
+	/d/{ print $(1)*60*60*24 }
+	' 
+	)
+	
+	# check if time is bigger than period
+	echo "time sec ${time_sec}"
+	echo "period sec ${period_sec}"
+	if [[ ${time_sec} -lt ${period_sec} ]]; then
+		echo "time <${time}> should be bigger than period <${period}>"
+		USAGE
+		exit 3
+	fi
+	
+	# calculate total_count
+	total_count=$(expr ${time_sec} / ${period_sec})
+	residual=$(expr ${time_sec} % ${period_sec})
+
+	if [[ $residual -ne 0 ]];then
+		total_count=$(expr ${total_count} + 1)
+	fi
+
+}
+
+
 function FUNC_get_cmdline(){
 	
 	local proc_path=$1
 	local cmdline_path="${proc_path}/cmdline"
 	
 	if [[ ! -e ${cmdline_path} ]];then
-		echo "n"
+		echo "not exist"
 	elif [[ ! -r ${cmdline_path} ]]; then
-		echo "d"
+		echo "unreadable"
 	else
         cat ${cmdline_path} | tr "\0" " "
 	fi
@@ -88,9 +138,9 @@ function FUNC_get_exe(){
 	local exe_path="${proc_path}/exe"
 	
 	if [[ ! -e ${exe_path} ]];then
-		echo "n"
+		echo "not exist"
 	elif [[ ! -r ${exe_path} ]]; then 
-		echo "d"
+		echo "unreadable"
 	else 
 		echo $(ls -l ${exe_path} | awk 'NF==11{print $(NF-2) $(NF-1) $(NF)} NF==9{print $NF" -> (null)"}')
 	fi
@@ -102,9 +152,9 @@ function FUNC_get_nbr_fds(){
 	local fd_path="${proc_path}/fd"
 
 	if [[ ! -e ${fd_path} ]];then
-		echo "n"
+		echo "not exist"
 	elif [[ ! -r ${fd_path} ]]; then 
-		echo "d"
+		echo "unreadable"
 	else 
 		echo $(ls ${fd_path} | wc -w)
 	fi
@@ -117,9 +167,9 @@ function FUNC_get_nbr_tasks(){
 	local task_path="${proc_path}/task"
 
 	if [[ ! -e ${task_path} ]];then
-		echo "n"
+		echo "not exist"
 	elif [[ ! -r ${task_path} ]]; then 
-		echo "d"
+		echo "unreadable"
 	else 
 		echo $(ls ${task_path} | wc -w)
 	fi
@@ -132,9 +182,9 @@ function FUNC_get_vmrss(){
 	local status_path="${proc_path}/status"
 
 	if [[ ! -e ${status_path} ]];then
-		echo "n"
+		echo "not exist"
 	elif [[ ! -r ${status_path} ]]; then 
-		echo "d"
+		echo "unreadable"
 	else 
 		echo $(cat ${status_path} | awk '/^VmRSS/{ print $2 }')
 	fi
@@ -149,19 +199,14 @@ fi
 
 # << variable setting >>
 
-time=${time:-1}
-period=${period:-1}
+time=${time:-"1h"}
+period=${period:-"1m"}
 output_dir=${output_dir:-"/tmp/process_monitor_outputs"}
 
 # <<< main >>>
 
 # calculate total times that shot the process state
-total_count=$(expr ${time} \* 60 / ${period})
-residual=$(expr ${time} \* 60 % ${period})
-
-if [[ $residual -ne 0 ]];then
-    total_count=$(expr ${total_count} + 1)
-fi
+FUNC_get_total_count ${time} ${period}
 
 # create a output folder
 if [[ -d ${output_dir} ]]; then
@@ -179,8 +224,8 @@ touch ${output_dir}/log.txt
 echo "touch ${output_dir}/log.txt"
 
 {
-printf "%s,%s,\n" "time"        "${time} hr"
-printf "%s,%s,\n" "period"      "${period} min"
+printf "%s,%s,\n" "time"        "${time}"
+printf "%s,%s,\n" "period"      "${period}"
 printf "%s,%s,\n" "total count" "${total_count} times"
 } > ${output_dir}/log.txt
 
@@ -213,16 +258,32 @@ do
 	if [[ ! -f ${output_dir}/${pid}.csv ]];then
 	    	
 	    # get process information
+		exe="$(FUNC_get_exe ${proc})"
 	    cmdline="$(FUNC_get_cmdline ${proc})"
-            exe="$(FUNC_get_exe ${proc})"
-           	 	
-            # create a blank csv file
+		
+		# check exe 
+		if [[ "${exe}" == "not exist" ]]; then
+			continue
+		elif [[ "${exe}" == "unreadable" ]]; then
+			continue
+		fi
+		
+		# check cmdline and filter "sleep"
+		if [[ "${cmdline}" == "not exist" ]]; then
+			continue
+		elif [[ "${cmdline}" == "unreadable" ]]; then
+			continue
+		elif [[ "${cmdline}" =~ ^sleep ]]; then
+			continue
+		fi
+		
+		# create a blank csv file
 	    touch "${output_dir}/${pid}.csv"
 	    	
 	    # append header to csv file
 	    {
-	        printf "%s,%s,\n" "pid" ${pid} 
-	    	printf "%s,%s,\n" "exe" ${exe}
+	        printf "%s,%s,\n" "pid" "${pid}"
+	    	printf "%s,%s,\n" "exe" "${exe}"
 	    	printf "%s,%s,\n" "cmd"	"${cmdline}"		
 	    	printf "%s,%s,\n" "total count"	"${total_count}"
 	    	printf "%s,%s,%s,%s,%s,%s,\n" "date" "time" "count" "nbr_of_fds" "nbr_of_tasks" "vmrss" 
@@ -230,15 +291,15 @@ do
 	    } >> "${output_dir}/${pid}.csv"
 	else
 	    # append cotent to csv file
-            {
-	    	printf "%s,%s,%s,%s,%s,%s,\n" "$(date +"%D")" "$(date +"%R:%S")" "${count}" "${nbr_fds}" "${nbr_tasks}" "${vmrss}"
-            } >> "${output_dir}/${pid}.csv"
+		{
+		printf "%s,%s,%s,%s,%s,%s,\n" "$(date +"%D")" "$(date +"%R:%S")" "${count}" "${nbr_fds}" "${nbr_tasks}" "${vmrss}"
+		} >> "${output_dir}/${pid}.csv"
 	fi
 		
     done 
 
     # sleep
-    sleep "${period}m"
+    sleep "${period}"
 
 done 
 
